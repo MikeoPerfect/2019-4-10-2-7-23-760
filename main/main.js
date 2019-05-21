5061270c0b9f34d06b4d4dd4efca078c0a80191b
MikeoPerfect <hfuboys@outlook.com>
2019-05-22 00:12:00 +0800

    first commit

--- main/main.js
+++ main/main.js
@@ -1,4 +1,12 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+   const {distance = 0, parkTime = 0} = inputs;
+    let price = 6 + parkTime * 0.25;
+    if (distance >= 2 && distance <= 8) 
+        price = price + ((distance - 2) * 0.8);
+    else if(distance > 8) 
+        price = price + 6 * 0.8+ ((distance - 8) * 1.2);
+    price = Math.round(price);
+    return price;
 };
+
